2bff1486290dba69713063e2be7fa96e07d4748e
menezesr08 <menezesr08@gmail.com>
2020-02-18 21:10:40 +0000

    added initial plot for open and close prices

--- ReportGenerator.py
+++ ReportGenerator.py
@@ -4,6 +4,7 @@ from CryptoDataModel import CryptoDataModel
 import datetime
 import matplotlib.pyplot as plt
 
+
 class ReportGenerator:
     def __init__(self, data):
         self.data = data
@@ -40,10 +41,36 @@ class ReportGenerator:
         return bitcoin_prices, dates
 
     # TODO: think about different type of plots that we can add to pdf
-    def plot_weekly_btc_prices(self):
+    def plot_btc_prices(self):
         bitcoin_prices, dates = self.get_all_prices()
         plt.plot(dates, bitcoin_prices, 'go--', color='brown', linewidth=1, markersize=12)
         plt.ylabel('Price of a single bitcoin in dollars')
         plt.xlabel('Days of the week')
         plt.show()
 
+    def get_low_high_prices(self):
+        open_price = []
+        close_price = []
+        dates = []
+        for index, item in enumerate(self.data):
+            open_price.append(int(item.open))
+            close_price.append(int(item.close))
+
+        for index, item in enumerate(self.data):
+            timestamp = (int(item.time))
+            dates.append(datetime.datetime.fromtimestamp(timestamp))
+
+        dates = [(date.strftime("%a") + ' ' + str(date.day)) for date in dates]
+
+        return open_price, close_price, dates
+
+    def plot_open_low_prices(self):
+        open_prices, close_prices, dates = self.get_low_high_prices()
+        fig, axs = plt.subplots(2, constrained_layout=True)
+        axs[0].plot(dates, open_prices)
+        axs[0].set_title('Open Prices')
+        axs[1].plot(dates, close_prices)
+        axs[1].set_title('Close Prices')
+        # plt.ylabel('Price of a single bitcoin in dollars')
+        # plt.xlabel('Days of the week')
+        plt.show()
