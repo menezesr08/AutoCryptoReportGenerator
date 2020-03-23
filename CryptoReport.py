425ec5e2226424ef4ac05963ebe5ce7faeaadf9d
menezesr08 <menezesr08@gmail.com>
2020-03-23 14:21:46 +0000

    - got data in dataframe - changed functions to suit dataframe operations

--- CryptoReport.py
+++ CryptoReport.py
@@ -5,48 +5,57 @@
 # SubTodos for this task
 # --------------------------------------------------------------------
 import requests
+import pandas as pd
 import json
 
 from CryptoDataModel import CryptoDataModel
-from ReportGenerator import ReportGenerator
 from requests.exceptions import Timeout
 
 
 class CryptoReport:
-    def __init__(self, days):
+    def __init__(self, options):
         self.api_key = '738510752db4953d28dfc15ff4da8812af46d98dd961da115924cc5933ceb808'
-        self.weeklyBTCPricesURL = f'https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit={days}&api_key={self.api_key}'
+        self.list_of_currencies = options
 
-    # Todo: getting data from api is slow. Difficult to find a fix. Keep researching. (Not important task atm)
-    def get_data_from_api(self):
-        try:
-            response = requests.get(self.weeklyBTCPricesURL, timeout=1)
-        except Timeout:
-            print('The request timed out')
-        else:
-            return response.json()
+    def get_crypto_historical_data(self):
+        list_crypto_data = []
+        for currency in self.list_of_currencies:
+            url = f'https://min-api.cryptocompare.com/data/v2/histoday?fsym={currency}&tsym=USD&limit=7&api_key={self.api_key}'
+            response = requests.get(url).json()
+            outer_level = response['Data']
+            df = pd.DataFrame(outer_level['Data'])
+            df.to_pickle('historical_data.py')
+            list_crypto_data.append(df)
 
-    # Parse json data to get relevant information
-    def parse_json_data(self):
-        bitcoin_data = self.get_data_from_api()
-        outer_level_data = bitcoin_data['Data']
-        inner_level_data = outer_level_data['Data']
-        return inner_level_data
+        return list_crypto_data
 
-    def get_crypto_data(self):
-        # returns a list of objects
-        json_data = self.parse_json_data()
-        btc_data_list = []
-        for item in json_data:
-            data = CryptoDataModel(item)
-            btc_data_list.append(data)
+    # # Todo: getting data from api is slow. Difficult to find a fix. Keep researching. (Not important task atm)
+    # def get_data_from_api(self):
+    #     try:
+    #         response = requests.get(self.weeklyBTCPricesURL, timeout=1)
+    #     except Timeout:
+    #         print('The request timed out')
+    #     else:
+    #         return response.json()
+    #
+    # # Parse json data to get relevant information
+    # def parse_json_data(self):
+    #     bitcoin_data = self.get_data_from_api()
+    #     outer_level_data = bitcoin_data['Data']
+    #     inner_level_data = outer_level_data['Data']
+    #     return inner_level_data
+    #
+    # def get_crypto_data(self):
+    #     # returns a list of objects
+    #     json_data = self.parse_json_data()
+    #     btc_data_list = []
+    #     for item in json_data:
+    #         data = CryptoDataModel(item)
+    #         btc_data_list.append(data)
+    #
+    #     return btc_data_list
+    #
+    # def create_report(self):
+    #     report = ReportGenerator(self.get_crypto_data())
+    #     report.plot_volumefrom_volumeto_price()
 
-        return btc_data_list
-
-    def create_report(self):
-        report = ReportGenerator(self.get_crypto_data())
-        report.plot_volumefrom_volumeto_price()
-
-
-report = CryptoReport(7)
-report.create_report()
--- ReportGenerator.py
+++ ReportGenerator.py
@@ -1,78 +1,76 @@
-from functools import reduce
-
-from BarPlot import BarPlot
-from CryptoDataModel import CryptoDataModel
-import datetime
-import numpy as np
-import matplotlib.pyplot as plt
+from CryptoReport import CryptoReport
+from HistoricalDataFormatter import HistoricalDataFormatter
+import pandas as pd
+from Plots.BarPlot import BarPlot
 import Helper
-from DoubleLinePlot import DoubleLinePlot
-from LinePlot import LinePlot
+from Plots.DoubleLinePlot import DoubleLinePlot
+from Plots.LinePlot import LinePlot
 
+
+# class ReportGenerator:
+#     def __init__(self, data):
+#         self.data = data
+#         self.dates = self.get_all_dates()
+#
+#     def average_bitcoin_price(self):
+#         price = int(sum(daily_stats.close for daily_stats in self.data)) / len(self.data)
+#         average = []
+#         return price
+#
+#     def get_all_prices(self):
+#         bitcoin_prices = []
+#         for item in self.data:
+#             bitcoin_prices.append(int(item.close))
+#
+#         return bitcoin_prices
+#
+#     # The following selects part of the JSON data as requested.
+#     # The method takes any two attributes as parameters and returns all the data matching these attributes
+#     def get_specific_bitcoin_data(self, first_attribute, second_attribute):
+#         first_attribute_values = []
+#         second_attribute_values = []
+#         for item in self.data:
+#             first_attribute_values.append(int(getattr(item, first_attribute)))
+#             second_attribute_values.append(int(getattr(item, second_attribute)))
+#
+#         return first_attribute_values, second_attribute_values
+#
+#     def plot_open_close_prices(self):
+#         open_prices, close_prices = self.get_specific_bitcoin_data('open', 'close')
+#         bar_plot = BarPlot(self.get_all_dates(), open_prices, close_prices)
+#         bar_plot.plot_open_close_prices()
+#
+#     def plot_high_low_prices(self):
+#         low_prices, high_prices = self.get_specific_bitcoin_data('high', 'low')
+#         bar_plot = BarPlot(self.get_all_dates(), high_prices, low_prices)
+#         bar_plot.plot_high_low_prices()
+#
+#     def plot_volumefrom_volumeto_price(self):
+#         volume_from, volume_to = self.get_specific_bitcoin_data('volumefrom', 'volumeto')
+#         subplot = DoubleLinePlot(self.get_all_dates(), volume_from, volume_to)
+#         subplot.plot_volumefrom_volumeto_prices()
+#
+#     def plot_btc_prices(self):
+#         line_plot = LinePlot(self.get_all_dates(), self.get_all_prices())
+#         line_plot.plot_bitcoin_price()
+#
+#     def get_all_dates(self):
+#         dates = []
+#         for item in self.data:
+#             timestamp = (int(item.time))
+#             dates.append(Helper.get_date(timestamp))
+#
+#         dates = [Helper.format_date(date) for date in dates]
+#         return dates
 
 class ReportGenerator:
-    def __init__(self, data):
-        self.data = data
-        self.dates = self.get_all_dates()
+    def __init__(self, *options):
+        self.options = [option for option in options]
+        self.api = CryptoReport(self.options)
+        self.df = pd.read_pickle('historical_data.py')
+        self.list_df = []
+        self.list_df.append(self.df)
+        HistoricalDataFormatter(self.list_df).create_plots()
 
-    def average_bitcoin_price(self):
-        price = int(sum(daily_stats.close for daily_stats in self.data)) / len(self.data)
-        average = []
-        return price
 
-    def get_all_prices(self):
-        bitcoin_prices = []
-        for item in self.data:
-            bitcoin_prices.append(int(item.close))
-
-        return bitcoin_prices
-
-    # The following selects part of the JSON data as requested.
-    # The method takes any two attributes as parameters and returns all the data matching these attributes
-    def get_specific_bitcoin_data(self, first_attribute, second_attribute):
-        first_attribute_values = []
-        second_attribute_values = []
-        for item in self.data:
-            first_attribute_values.append(int(getattr(item, first_attribute)))
-            second_attribute_values.append(int(getattr(item, second_attribute)))
-
-        return first_attribute_values, second_attribute_values
-
-    def plot_open_close_prices(self):
-        open_prices, close_prices = self.get_specific_bitcoin_data('open', 'close')
-        bar_plot = BarPlot(self.get_all_dates(), open_prices, close_prices)
-        bar_plot.plot_open_close_prices()
-
-    def plot_high_low_prices(self):
-        low_prices, high_prices = self.get_specific_bitcoin_data('high', 'low')
-        bar_plot = BarPlot(self.get_all_dates(), high_prices, low_prices)
-        bar_plot.plot_high_low_prices()
-
-    def plot_volumefrom_volumeto_price(self):
-        volume_from, volume_to = self.get_specific_bitcoin_data('volumefrom', 'volumeto')
-        subplot = DoubleLinePlot(self.get_all_dates(), volume_from, volume_to)
-        subplot.plot_volumefrom_volumeto_prices()
-
-    def plot_btc_prices(self):
-        line_plot = LinePlot(self.get_all_dates(), self.get_all_prices())
-        line_plot.plot_bitcoin_price()
-
-    def get_all_dates(self):
-        dates = []
-        for item in self.data:
-            timestamp = (int(item.time))
-            dates.append(Helper.get_date(timestamp))
-
-        dates = [Helper.format_date(date) for date in dates]
-        return dates
-
-    @staticmethod
-    def human_format(num):
-        num = float('{:.3g}'.format(num))
-        magnitude = 0
-        while abs(num) >= 1000:
-            magnitude += 1
-            num /= 1000.0
-        return '{}{}'.format('{:f}'.format(num).rstrip('0').rstrip('.'), ['', 'K', 'M', 'B', 'T'][magnitude])
-
-    # Todo: Hide Api Key
+report = ReportGenerator("BTC")
