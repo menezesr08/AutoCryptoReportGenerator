f469afc0dbfc448c13121643c1517ca59358b439
menezesr08 <menezesr08@gmail.com>
2020-06-03 09:59:42 +0100

    added additional files for the previous commit

---
+++ enums/PlotLabels.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class PlotLabels(Enum):
+    monthly_plot_title = "Bitcoin prices for the last 30 days"
+    monthly_plot_x = "Weekly dates for the last 30 days"
+
+
+
+
+
+    y_title = "Bitcoin price"
