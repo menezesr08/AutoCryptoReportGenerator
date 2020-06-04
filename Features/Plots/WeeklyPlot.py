21ce639084977c0a67f936fc95b5930c2746bb4e
menezesr08 <menezesr08@gmail.com>
2020-06-04 11:12:13 +0100

    finished weekly plot (1st iteration)

--- Features/Plots/WeeklyPlot.py
+++ Features/Plots/WeeklyPlot.py
@@ -12,44 +12,69 @@ from enums.PlotLabels import PlotLabels
 class WeeklyPlot:
 
     def __init__(self, crypto_data_dict, simple_moving_average, exponential_moving_average):
-        self.crypto_data_dict = crypto_data_dict
+        self.title = crypto_data_dict['title']
+        self.window = crypto_data_dict['window']
+
         self.sma = simple_moving_average
         self.ema = exponential_moving_average
-        self.fig = plt.figure()
 
-        self.ax = self.fig.add_subplot(111)
-        self.ax.grid(True)
-        # self.ax.xaxis.set_minor_locator(mdates.DayLocator(interval=1))
-        self.ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=MO))
-        self.ax.xaxis.set_major_formatter(mdates.DateFormatter('%a %d %b'))
+        self.formatted_data = self.format_data(crypto_data_dict['data'])
+        self.weekly_min_prices, self.weekly_max_prices, self.simple_moving_average, self.exponential_moving_average = \
+            self.calculate_statistics(self.formatted_data)
+
+        self.ax, self.fig = self.initialise_plot()
 
     def create_plot(self):
-        formatted_data = self.format_data(self.crypto_data_dict)
-        weekly_min_prices, weekly_max_prices, simple_moving_average, exponential_moving_average = \
-            self.calculate_statistics(formatted_data)
-        x = formatted_data['time']
-        y = formatted_data['close']
-        self.ax.plot(x, y)
-        self.ax.plot(formatted_data['time'], simple_moving_average, color='blue')
-        self.ax.plot(formatted_data['time'], exponential_moving_average, color='red')
-        self.ax.scatter(weekly_max_prices['time'], weekly_max_prices['close'], marker="^", color='blue')
-        self.ax.scatter(weekly_min_prices['time'], weekly_min_prices['close'], marker="^", color='red')
-        self.fig.autofmt_xdate()
-        plt.title(PlotLabels.monthly_plot_title.value)
-        plt.xlabel(PlotLabels.monthly_plot_x.value)
-        plt.ylabel(PlotLabels.y_title.value)
+        self.plot_lines()
+        self.apply_labels()
+        self.apply_legend()
         plt.show()
         # plt.savefig('images/plot - {0}'.format(Helper.todays_date()))
+
+    def initialise_plot(self):
+        plt.style.use('seaborn-darkgrid')
+        my_dpi = 96
+        fig = plt.figure(figsize=(480 / my_dpi, 480 / my_dpi), dpi=my_dpi)
+        ax = fig.add_subplot(111)
+        ax.grid(True)
+        ax.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=MO))
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%a %d %b'))
+        return ax, fig
+
+    def plot_lines(self):
+        self.ax.plot(self.formatted_data['time'], self.formatted_data['close'], marker='', color='orange', linewidth=4,
+                     alpha=0.3)
+        self.ax.plot(self.formatted_data['time'], self.simple_moving_average, color='red', linewidth=1, alpha=0.5,
+                     label="Simple moving average")
+        self.ax.plot(self.formatted_data['time'], self.exponential_moving_average, color='blue', linewidth=1, alpha=0.5,
+                     label="Exponential moving average")
+        self.ax.scatter(self.weekly_max_prices['time'], self.weekly_max_prices['close'], marker="^", color='black',
+                        label="Max price for each week")
+        self.ax.scatter(self.weekly_min_prices['time'], self.weekly_min_prices['close'], marker="v", color='red',
+                        label="Min price for each week")
+        self.fig.autofmt_xdate()
+
+    def apply_labels(self):
+        self.ax.set_title(PlotLabels.monthly_plot_title.value)
+        self.ax.set_xlabel(PlotLabels.monthly_plot_x.value)
+        self.ax.set_ylabel(PlotLabels.y_title.value)
+        self.ax.xaxis.labelpad = 40
+        self.ax.yaxis.labelpad = 4
+
+    def apply_legend(self):
+        box = self.ax.get_position()
+        self.ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+        # Put a legend to the right of the current axis
+        self.ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
     def calculate_statistics(self, formatted_data):
         weekly_min_prices = self.min_close_per_week(formatted_data[['time', 'close']])
         weekly_max_prices = self.max_close_per_week(formatted_data[['time', 'close']])
-        simple_moving_average = self.sma(formatted_data['close'], self.crypto_data_dict['window'].value)
-        exponential_moving_average = self.ema(formatted_data['close'], self.crypto_data_dict['window'].value)
+        simple_moving_average = self.sma(formatted_data['close'], self.window.value)
+        exponential_moving_average = self.ema(formatted_data['close'], self.window.value)
         return weekly_min_prices, weekly_max_prices, simple_moving_average, exponential_moving_average
 
-    def format_data(self, crypto_data_dict):
-        data = crypto_data_dict['data']
+    def format_data(self, data):
         data['time'] = data['time'].apply(Helper.convert_to_date)
         return data
 
--- enums/PlotLabels.py
+++ enums/PlotLabels.py
@@ -5,8 +5,4 @@ class PlotLabels(Enum):
     monthly_plot_title = "Bitcoin prices for the last 30 days"
     monthly_plot_x = "Weekly dates for the last 30 days"
 
-
-
-
-
-    y_title = "Bitcoin price"
+    y_title = "Bitcoin price ($)"
