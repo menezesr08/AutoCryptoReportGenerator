df929ad4638ce9cdeefb8546323d81841c48415b
menezesr08 <menezesr08@gmail.com>
2020-06-29 18:03:39 +0100

    added redis production url

--- worker.py
+++ worker.py
@@ -3,7 +3,8 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = 'redis://localhost:6379'
+# redis_url = 'redis://localhost:6379'
+redis_url = 'redis://redistogo:e9c37ff2c6f85f431a4b6d12a5557c5a@soapfish.redistogo.com:9722'
 
 conn = redis.from_url(redis_url)
 
